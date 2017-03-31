0e31e76f4446e61cd2b489057ead284e96b616a6
LiHao <haoli@rtigertech.com>
2017-03-31 16:28:01 +0800

    init 初始化 截图、拍照保存、rxjava、glide、bluetooth

---
+++ RxJavaDemo/app/src/main/java/com/trust/rxjavademo/GitHubInterface.java
@@ -0,0 +1,28 @@
+package com.trust.rxjavademo;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by Trust on 2017/3/21.
+ */
+public interface GitHubInterface {
+    @GET("{owner}/{repo}/{repos}")
+    Call<ResponseBody> contributorsBySimpleGetCall(@Path("owner") String owner,
+                                                   @Path("repo") String repo,
+                                                   @Path("repos") String repos);
+
+//    @Headers({"Content-Type: application/json","Accept: application/json"})//需要添加头
+//    @FormUrlEncoded
+    @POST("rest/gps/latest/")
+    Call<ResponseBody> postResult(@Body RequestBody route);
+
+}
