f4ef97ec385a7dc5b7d3da90e0e443eea23a80ff
TrustMe <zouzou1111@qq.com>
2017-04-16 18:23:57 +0800

    update

---
+++ RxJavaDemo/app/src/main/java/com/trust/rxjavademo/RxActivity.java
@@ -0,0 +1,13 @@
+package com.trust.rxjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx);
+    }
+}
